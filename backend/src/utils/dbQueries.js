49152a5588911d5af2e28e5a656e9789bc321048
calfmike <juan.calfa@hotmail.com>
2024-06-23 00:36:48 -0300

    sanitize queries

--- backend/src/utils/dbQueries.js
+++ backend/src/utils/dbQueries.js
@@ -4,11 +4,11 @@ const Admin = require('../models/Admin');  // Asegúrate de importar el modelo A
 const Transaction = require('../models/Transaction');
 
 async function findUserByEmail(email) {
-    return User.findOne({ email });
+    return User.findOne({ email: sanitize(email) });
 }
 
 async function findUserById(id) {
-    return User.findById(id);
+    return User.findById(sanitize(id));
 }
 
 async function updateUser(user) {
@@ -16,33 +16,46 @@ async function updateUser(user) {
 }
 
 async function createAccount(accountData) {
-    const account = new Account(accountData);
+    const account = new Account(sanitize(accountData));
     return account.save();
 }
 
 async function findAccountByIdentifier(identifier) {
     return Account.findOne({
-        $or: [{ cbu: identifier }, { alias: identifier }]
+        $or: [{ cbu: sanitize(identifier) }, { alias: sanitize(identifier) }]
     });
 }
 
 async function createTransaction(transactionData) {
-    const transaction = new Transaction(transactionData);
+    const transaction = new Transaction(sanitize(transactionData));
     return transaction.save();
 }
 
 async function getTransactionsByAccountId(accountId) {
     return Transaction.find({
-        $or: [{ fromAccount: accountId }, { toAccount: accountId }]
+        $or: [{ fromAccount: sanitize(accountId) }, { toAccount: sanitize(accountId) }]
     });
 }
 
 async function findAdminByUsername(username) {
-    return Admin.findOne({ username });
+    return Admin.findOne({ username: sanitize(username) });
 }
 
 async function findAllUsers() {
     return User.find();
+}
+
+function sanitize(input) {
+    if (typeof input === 'string') {
+        return input.replace(/[^\w\s.-_@]/gi, ''); // Limpiar cadena de caracteres especiales
+    }
+    if (typeof input === 'object' && input !== null) {
+        return Object.keys(input).reduce((acc, key) => {
+            acc[key] = sanitize(input[key]);
+            return acc;
+        }, {});
+    }
+    return input;
 }
 
 module.exports = {
